ea6eb71762753a8d89e10174e12d22d30b01bb8f
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-08-25 18:55:03 +0900

    #2 initialize spring boot project

---
+++ simple-spring-rest-api/src/main/java/tech/ryuichi24/simplespringrestapi/SimpleSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package tech.ryuichi24.simplespringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ simple-spring-rest-api/src/test/java/tech/ryuichi24/simplespringrestapi/SimpleSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ryuichi24.simplespringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
