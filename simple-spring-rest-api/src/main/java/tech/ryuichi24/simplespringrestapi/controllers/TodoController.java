4c83e30c816b3456153bf17ba531725747584419
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-08-27 06:03:03 +0000

    refactor TodoController

--- simple-spring-rest-api/src/main/java/tech/ryuichi24/simplespringrestapi/controllers/TodoController.java
+++ simple-spring-rest-api/src/main/java/tech/ryuichi24/simplespringrestapi/controllers/TodoController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,19 +26,19 @@ public class TodoController {
     @Autowired
     private TodoService _todoService;
 
-    @RequestMapping(method = RequestMethod.GET, path = "")
+    @GetMapping(path = "")
     public ResponseEntity<List<TodoItem>> getTodoItems() {
         List<TodoItem> todoItems = _todoService.getTodoItems();
         return ResponseEntity.ok(todoItems);
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<TodoItem> getTodoItem(@PathVariable int id) {
         TodoItem found = _todoService.getTodoItemById(id);
         return ResponseEntity.ok(found);
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "")
+    @PostMapping(path = "")
     public ResponseEntity<TodoItem> createTodoItem(@RequestBody TodoItem newTodoItem) {
         TodoItem savedTodoItem = _todoService.saveTodoItem(newTodoItem);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -44,13 +47,13 @@ public class TodoController {
         return ResponseEntity.created(location).body(savedTodoItem);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    @PutMapping(path = "/{id}")
     public ResponseEntity<?> updateTodoItem(@PathVariable int id, @RequestBody TodoItem newTodoItem) {
         _todoService.updateTodoItem(id, newTodoItem);
         return ResponseEntity.noContent().build();
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<?> removeTodoItem(@PathVariable int id) {
         _todoService.removeTodoItemById(id);
         return ResponseEntity.noContent().build();
