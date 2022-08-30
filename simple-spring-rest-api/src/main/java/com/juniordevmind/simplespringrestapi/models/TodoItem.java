49f4defae4af17b81243d1d8910b2c5d387dbbb4
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-08-30 05:41:16 +0000

    add createdAt and updatedAt to TodoModel

--- simple-spring-rest-api/src/main/java/com/juniordevmind/simplespringrestapi/models/TodoItem.java
+++ simple-spring-rest-api/src/main/java/com/juniordevmind/simplespringrestapi/models/TodoItem.java
@@ -1,10 +1,15 @@
 package com.juniordevmind.simplespringrestapi.models;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -26,4 +31,23 @@ public class TodoItem {
     @Column(name = "title", nullable = false)
     @NotBlank(message = "Title must not be blank.")
     private String title;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void PrePersist() {
+        if (Objects.isNull(this.createdAt))
+            this.createdAt = LocalDateTime.now();
+        if (Objects.isNull(this.updatedAt))
+            this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
